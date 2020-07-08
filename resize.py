3cef8de8e5e96363506eabdd25a5e86eea3c05a3
Satrio Adi Rukmono <r.satrioadi@gmail.com>
2020-07-08 17:23:06 +0700

    Adjust resize to match specification.

--- resize.py
+++ resize.py
@@ -7,6 +7,52 @@ y = int(sys.argv[2])
 
 for fname in sys.argv[3:]:
     im1 = Image.open(fname)
+    pixelMap = im1.load()
+
+    imin = 0
+    imax = im1.size[0] - 1
+    jmin = 0
+    jmax = im1.size[1] - 1
+    for i in range(im1.size[0]):
+        for j in range(im1.size[1]):
+            t = pixelMap[i, j][3]
+            if t > 100:
+                imin = max(imin, i - 1)
+                break
+        else:
+            continue
+        break
+    for i in reversed(range(im1.size[0])):
+        for j in range(im1.size[1]):
+            t = pixelMap[i, j][3]
+            if t > 100:
+                imax = min(imax, i + 1)
+                break
+        else:
+            continue
+        break
+    for j in range(im1.size[1]):
+        for i in range(im1.size[0]):
+            t = pixelMap[i, j][3]
+            if t > 100:
+                jmin = max(jmin, j - 1)
+                break
+        else:
+            continue
+        break
+    for j in reversed(range(im1.size[1])):
+        for i in range(im1.size[0]):
+            t = pixelMap[i, j][3]
+            if t > 100:
+                jmax = min(jmax, j + 1)
+                break
+        else:
+            continue
+        break
+
+    im1 = im1.crop((imin, jmin, imax, jmax))
+
+    im1.thumbnail((x,y), Image.ANTIALIAS)
     im2 = Image.new(im1.mode, (x, y))
     x1 = (x-im1.width)//2
     y1 = (y-im1.height)//2
